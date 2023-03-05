5e17d7ea3f89e177cc6d58da45eff09b061c9cbf
Natali <natalagurban7@gmail.com>
2023-03-05 17:05:27 +0300

    Решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+Console.Clear();
+Console.WriteLine(" Введи цифру: ");
+int dayNumber=int.Parse(Console.ReadLine()!);
+if (dayNumber==6||dayNumber==7)
+{
+    Console.Write($"Этот день недели выходной");
+}
+else if (dayNumber<1||dayNumber>7)
+{
+    Console.Write("Нет такого дня недели");
+}
+else
+{
+    Console.Write("Не выходной");
+}
